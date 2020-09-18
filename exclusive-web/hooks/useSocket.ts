0d45fd9a6466aaa8cf8434bd084025cc038e9817
Luiz Chagas <luizchagasjr@gmail.com>
2020-09-18 15:04:07 -0500

    Update useSocket.ts

--- exclusive-web/hooks/useSocket.ts
+++ exclusive-web/hooks/useSocket.ts
@@ -5,7 +5,7 @@ export const useSocket = () => {
   const [lastMessage, setLastMessage] = useState('')
 
   useEffect(() => {
-    const socket = io('http://35.188.69.83', {
+    const socket = io('https://35.188.69.83:8080', {
       transports: ['websocket'],
     })
     socket.on('event', (message: string) => {
